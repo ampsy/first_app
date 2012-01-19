fbe4c36cf0f2164c377d6c74cc7a289e0ba08d40
Jeremy Gocke <jeremy@fliptu.com>
2012-01-18 23:42:45 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'f7b0de73de493ea04d7efee178608a301a6ba8766d237a0d2f91864cdc514b6b534e95d46defc3973fc5bda11b23e7d06920c8e16a2781d14bb836fc60c303a7'
